b8c16e0c57590bbe54c7e5bc2e82b3612a6f04be
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-10 21:17:08 +0700

    User

--- src/startDatabase.ts
+++ src/startDatabase.ts
@@ -1,8 +1,5 @@
 import { connect } from 'mongoose';
 import mongoose = require('mongoose');
-import { User } from './models/User';
-
-User.ensureIndexes();
 
 mongoose.Promise = global.Promise;
 connect('mongodb://localhost/fb1808', { useMongoClient: true });
